b78bb295bb735a0127d95b20617225e9ee2925ef
Erick Souza <erick_souza2104@outlook.com>
2023-12-17 19:07:13 -0300

    Update Button.test.js

--- src/atoms/Button.test.js
+++ src/atoms/Button.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {screen,fireEvent } from '@testing-library/react';
 import { render } from "@testing-library/react";
-import "@testing-library/jest-dom"
 import ThemeProvider from "../styles/ThemeProvider";
 
 import Button from "./Button"
